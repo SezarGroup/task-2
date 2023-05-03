1515869a7df839fcef3189342c47702c01804658
SergeevSezar <sergeev.sezar@yandex.ru>
2023-05-03 15:57:56 +0900

    complete task 2-1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,16 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld helloWorld1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld helloWorld2 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(helloWorld1.getMessage());
+        System.out.println(helloWorld2.getMessage());
+        System.out.println(cat1.getName());
+        System.out.println(cat2.getName());
+        System.out.println("Две пары Hello World = " + helloWorld1.equals(helloWorld2));
+        System.out.println("Две пары Cat = " + cat1.equals(cat2));
     }
 }
